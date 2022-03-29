84eb8965a3cf0919a9928aaf3a2aa50effe7e6a3
Promise Dash <86062880+promisedash@users.noreply.github.com>
2022-03-29 13:04:19 +0530

    Delete app directory

--- app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userSlice from '../features/userSlice';
-
-export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
-});
